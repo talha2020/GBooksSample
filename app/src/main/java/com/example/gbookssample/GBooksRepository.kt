0b6e718f92aa79e328103f7ae10c4abc3fa8cd58
Talha <talha.mir@foodie.fm>
2020-02-23 22:14:04 +0200

    Added some comments.

--- app/src/main/java/com/example/gbookssample/GBooksRepository.kt
+++ app/src/main/java/com/example/gbookssample/GBooksRepository.kt
@@ -6,6 +6,7 @@ import javax.inject.Inject
 
 class GBooksRepository @Inject constructor(private val apiClient: ApiClient) {
 
+    // In bigger projects, having interfaces for repositories and apiclient can be helpful - kept it simple here.
     suspend fun getBooks(query: String): ApiResponse<Volume> {
         return apiClient.getBooks(query)
         // Here we can do further tasks if needed like saving/caching data or accessing user credentials and stuff.
